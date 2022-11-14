7e0a75c5245dda4acad84478a3f85eddb1e4bc42
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-14 13:21:10 +0200

    init

---
+++ dist/index.579125c3.js
@@ -0,0 +1,15 @@
+const field = document.querySelector(".field");
+const form = document.querySelector(".form");
+form.addEventListener("submit", handleSubmit);
+createFieldies(9);
+function handleSubmit(e) {
+    e.preventDefault();
+    const { elements: { player1 , player2  }  } = e.target;
+}
+function createFieldies(num) {
+    const marcup = [];
+    for(let i = 1; i <= num; i += 1)marcup.push(`<div class='fieldy' id='${i}'></div>`);
+    field.innerHTML = marcup.join("");
+}
+
+//# sourceMappingURL=index.579125c3.js.map
