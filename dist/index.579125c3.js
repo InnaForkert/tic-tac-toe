2964c6cf8901131ab9e0923ec3f782d1677ebcff
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2022-11-17 23:28:21 +0200

    steps and playernames

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -11,14 +11,46 @@ var player1GameName = document.querySelector("#player1Name");
 var player2GameName = document.querySelector("#player2Name");
 var fieldies = document.querySelectorAll(".fieldy");
 var winningCombos = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
+    [
+        1,
+        2,
+        3
+    ],
+    [
+        4,
+        5,
+        6
+    ],
+    [
+        7,
+        8,
+        9
+    ],
+    [
+        1,
+        4,
+        7
+    ],
+    [
+        2,
+        5,
+        8
+    ],
+    [
+        3,
+        6,
+        9
+    ],
+    [
+        1,
+        5,
+        9
+    ],
+    [
+        3,
+        5,
+        7
+    ], 
 ];
 var currentComboX = [];
 var currentComboO = [];
@@ -26,17 +58,17 @@ var currentSymbol = "X";
 var activePlayer = "";
 var player1Name;
 var player2Name;
-if (form)
-    form.addEventListener("submit", handleSubmit);
-fieldies.forEach(function (fieldy) {
+if (form) form.addEventListener("submit", handleSubmit);
+fieldies.forEach(function(fieldy) {
     fieldy.addEventListener("click", handlePlayerMove);
 });
 function checkForVictory(arr) {
-    var victory = winningCombos.find(function (combo) {
-        return combo.every(function (num) { return arr.includes(num); });
+    var victory = winningCombos.find(function(combo) {
+        return combo.every(function(num) {
+            return arr.includes(num);
+        });
     });
-    if (victory)
-        alert("Victory!");
+    if (victory) alert("Victory!");
 }
 function handlePlayerMove(e) {
     var target = e.target;
@@ -45,32 +77,27 @@ function handlePlayerMove(e) {
     if (currentSymbol === "X") {
         currentComboX.push(Number(target.dataset.id));
         checkForVictory(currentComboX);
-    }
-    else {
+    } else {
         currentComboO.push(Number(target.dataset.id));
         checkForVictory(currentComboO);
     }
     currentSymbol = currentSymbol === "X" ? "O" : "X";
-    player1GameName === null || player1GameName === void 0 ? void 0 : player1GameName.classList.toggle("active-player");
-    player2GameName === null || player2GameName === void 0 ? void 0 : player2GameName.classList.toggle("active-player");
+    player1GameName === null || player1GameName === void 0 || player1GameName.classList.toggle("active-player");
+    player2GameName === null || player2GameName === void 0 || player2GameName.classList.toggle("active-player");
     swapPlayers();
 }
 function swapPlayers() {
-    if (activePlayer === "" || player1Name) {
-        activePlayer = player2Name;
-    }
-    else {
-        activePlayer = player1Name;
-    }
+    if (activePlayer === "" || player1Name) activePlayer = player2Name;
+    else activePlayer = player1Name;
 }
 function handleSubmit(e) {
     e.preventDefault();
     player1Name = (player1 === null || player1 === void 0 ? void 0 : player1.value) || "Player1";
     player2Name = (player2 === null || player2 === void 0 ? void 0 : player2.value) || "Player2";
-    menu === null || menu === void 0 ? void 0 : menu.classList.add("visually-hidden");
-    field === null || field === void 0 ? void 0 : field.classList.remove("visually-hidden");
-    if (player1GameName)
-        player1GameName.innerText = player1Name;
-    if (player2GameName)
-        player2GameName.innerText = player2Name;
+    menu === null || menu === void 0 || menu.classList.add("visually-hidden");
+    field === null || field === void 0 || field.classList.remove("visually-hidden");
+    if (player1GameName) player1GameName.innerText = player1Name;
+    if (player2GameName) player2GameName.innerText = player2Name;
 }
+
+//# sourceMappingURL=index.579125c3.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,76 @@
-export const constForExport: string = "";
+export const stuf = [];
 
+const menu: HTMLElement | null = document.querySelector(".menu");
 const field: HTMLElement | null = document.querySelector(".field");
 const form: HTMLElement | null = document.querySelector(".form");
 const player1: HTMLInputElement | null = document.querySelector("#player1");
 const player2: HTMLInputElement | null = document.querySelector("#player2");
+const player1GameName: HTMLElement | null =
+  document.querySelector("#player1Name");
+const player2GameName: HTMLElement | null =
+  document.querySelector("#player2Name");
+const fieldies: NodeListOf<HTMLElement> = document.querySelectorAll(".fieldy");
+const winningCombos = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+const currentComboX: number[] = [];
+const currentComboO: number[] = [];
+
+let currentSymbol: string = "X";
+let activePlayer: string = "";
+let player1Name: string;
+let player2Name: string;
 
 if (form) form.addEventListener("submit", handleSubmit);
+fieldies.forEach((fieldy) => {
+  fieldy.addEventListener("click", handlePlayerMove);
+});
+
+function checkForVictory(arr: number[]) {
+  const victory = winningCombos.find((combo) =>
+    combo.every((num) => arr.includes(num))
+  );
+  if (victory) alert("Victory!");
+}
+
+function handlePlayerMove(e: MouseEvent) {
+  const target = e.target as HTMLElement;
+  target.innerText = currentSymbol;
+  target.removeEventListener("click", handlePlayerMove);
+  if (currentSymbol === "X") {
+    currentComboX.push(Number(target.dataset.id));
+    checkForVictory(currentComboX);
+  } else {
+    currentComboO.push(Number(target.dataset.id));
+    checkForVictory(currentComboO);
+  }
+  currentSymbol = currentSymbol === "X" ? "O" : "X";
+  player1GameName?.classList.toggle("active-player");
+  player2GameName?.classList.toggle("active-player");
+  swapPlayers();
+}
+
+function swapPlayers() {
+  if (activePlayer === "" || player1Name) {
+    activePlayer = player2Name;
+  } else {
+    activePlayer = player1Name;
+  }
+}
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const player1Name: string = player1?.value || "Player1";
-  const player2Name: string = player2?.value || "Player2";
+  player1Name = player1?.value || "Player1";
+  player2Name = player2?.value || "Player2";
+  menu?.classList.add("visually-hidden");
+  field?.classList.remove("visually-hidden");
+  if (player1GameName) player1GameName.innerText = player1Name;
+  if (player2GameName) player2GameName.innerText = player2Name;
 }
-
-(function createFieldies(num: number) {
-  const marcup: string[] = [];
-  for (let i = 1; i <= num; i += 1) {
-    marcup.push(`<div class='fieldy' id='${i}'></div>`);
-  }
-  field!.innerHTML = marcup.join("");
-})(9);
